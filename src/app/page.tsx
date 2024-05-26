7f075d88462f0be900570fd5ce8c2c6280723b24
Raghav Kaushik <raghav.kaushik611@gmail.com>
2024-05-26 14:15:41 +0530

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import './globals.css'
+
 export default function Home() {
+  const cells = new Array( 5 ).fill( '' ), rows = new Array( 6 ).fill( '' )
+
   return (
-    <main className='min-h-screen'>
-      Hello
+    <main className='h-screen flex flex-col'>
+      <header className='header'>WORDLE</header>
+
+      <section className='app-container'>
+        {
+          rows.map( ( r, i ) => (
+            <div className='row' key={ i }>
+              {cells.map( ( c, j ) => (
+                <div className='cell' key={ j }></div>
+              ) )}
+            </div>
+          ) )
+        }
+      </section>
     </main>
   )
 }
