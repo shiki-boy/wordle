123c98ee070d0c00399f30915d79d06e4cffd6ab
Raghav Kaushik <raghav.kaushik611@gmail.com>
2024-05-26 15:27:05 +0530

    zustand store updates working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,39 @@
+'use client'
+import { useCallback, useEffect } from 'react'
 import './globals.css'
+import { useWordleStore } from './store'
 
 export default function Home() {
+  const { chances, update, submit } = useWordleStore()
+
   const cells = new Array( 5 ).fill( '' ), rows = new Array( 6 ).fill( '' )
+
+  // console.log( 33 )
+
+  const handleKeyPress = useCallback( ( e: KeyboardEvent ) => {
+    if( /^[a-zA-Z]$/.test( e.key ) ) {
+      update( e.key )
+      return
+    }
+
+    if ( 'Enter' === e.key ) {
+      submit()
+      return
+    }
+
+    if ( 'Backspace' === e.key ) {
+      update( null )
+      return
+    }
+  }, [ update, submit ] )
+
+  useEffect( () => {
+    document.addEventListener( 'keyup', handleKeyPress )
+
+    return () => {
+      document.removeEventListener( 'keyup', handleKeyPress )
+    }
+  }, [ handleKeyPress ] )
 
   return (
     <main className='h-screen flex flex-col'>
@@ -12,7 +44,7 @@ export default function Home() {
           rows.map( ( r, i ) => (
             <div className='row' key={ i }>
               {cells.map( ( c, j ) => (
-                <div className='cell' key={ j }></div>
+                <div className='cell' key={ j }>{chances[i][j] ?? ''}</div>
               ) )}
             </div>
           ) )
---
+++ src/app/store.ts
@@ -0,0 +1,42 @@
+import { create } from 'zustand'
+
+interface StoreState {
+  chances: string[];
+  indexToUpdate: number;
+  gameOver: boolean;
+  update: (word: string | null) => void; // eslint-disable-line
+  submit: () => void;
+}
+
+export const useWordleStore = create<StoreState>()( ( set ) => ( {
+  chances: new Array( 6 ).fill( '' ),
+  gameOver: false,
+  indexToUpdate: 0,
+  submit: () =>
+    set( ( state ) => {
+      if ( 5 !== state.chances[state.indexToUpdate].length || 5 === state.indexToUpdate ) {
+        return state
+      }
+      return ( {
+        gameOver: 4 === state.indexToUpdate,
+        indexToUpdate: state.indexToUpdate + 1,
+      } )
+    } ),
+  update: ( letter: string | null ) =>
+    set( ( state ) => {
+      if ( 5 === state.chances[state.indexToUpdate].length ) {
+        return state
+      }
+
+      if ( !letter ) {
+        state.chances[state.indexToUpdate] = state.chances[
+          state.indexToUpdate
+        ].slice( state.chances[state.indexToUpdate].length - 1 )
+        return { chances: [ ...state.chances ] }
+      }
+
+      state.chances[state.indexToUpdate] += letter
+
+      return { chances: [ ...state.chances ] }
+    } ),
+} ) )
